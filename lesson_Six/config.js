c097e8cc14a1a6da4568714c628be407e3659f62
Mostafa Hassanzadeh <mostafaa.hasanzadeh@gmail.com>
2018-03-09 18:07:07 +0330

    Create config.js

---
+++ lesson_Six/config.js
@@ -0,0 +1,13 @@
+'use strict';
+const 
+    config ={
+    token: <token>,
+    botOption:{
+        autoUpdate: true,
+        autoChatAction: true,
+        autoChatActionUploadOnly: false,
+    },
+    botUrl: 'https://t.me/mostafhbot'
+    }
+;
+module.exports = config;
