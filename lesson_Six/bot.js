e8d8daec7df07bf6dbced7b830254291ec24c2b1
Mostafa Hassanzadeh <mostafaa.hasanzadeh@gmail.com>
2018-03-09 18:06:27 +0330

    Create bot.js

---
+++ lesson_Six/bot.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const
+config = require('./config'),
+TelegramBot = require('telegram-bot-api.js').default,
+bot = new TelegramBot(config.token,config.botOption)
+;
+module.exports = bot;
